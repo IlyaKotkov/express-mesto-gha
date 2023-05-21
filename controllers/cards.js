64fc84f85acb5d1b1c17cbc9b0b6ad0936b4b3a7
Ilya Kotkov <MopsBox2552@yandex.ru>
2023-05-21 19:25:57 +0300

    Добавить проверку прав

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,6 +1,7 @@
 const Card = require('../models/card');
 const NotFoundError = require('../errors/NotFoundError');
 const BadRequesError = require('../errors/BadRequesError');
+const ForbiddenError = require('../errors/ForbiddenError');
 
 const STATUS_OK = 200;
 
@@ -30,7 +31,12 @@ module.exports.deleteCardById = (req, res, next) => {
       newError.name = 'DocumentNotFoundError';
       throw newError;
     })
-    .then((card) => res.send(card))
+    .then((card) => {
+      if (card.owner !== req.user._id) {
+        throw new ForbiddenError('Недостаточно прав для этого действия');
+      }
+      return res.send(card);
+    })
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
         next(new NotFoundError('Карточка по указанному _id не найдена.'));
@@ -66,14 +72,16 @@ module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
   { $pull: { likes: req.user._id } },
   { new: true },
 )
-  .then((card) => {
-    if (!card) {
-      next(new NotFoundError('Карточка с указанным _id не найдена.'));
-    }
-    return res.status(STATUS_OK).send({ data: card });
+  .orFail(() => {
+    const newError = new Error();
+    newError.name = 'DocumentNotFoundError';
+    throw newError;
   })
+  .then((card) => { res.status(STATUS_OK).send({ data: card }); })
   .catch((err) => {
-    if (err.name === 'CastError') {
+    if (err.status === 'DocumentNotFoundError') {
+      next(new NotFoundError('Карточка с указанным _id не найдена.'));
+    } else if (err.name === 'CastError') {
       next(new BadRequesError('Переданы некорректные данные для постановки лайка.'));
     } else {
       next(err);
